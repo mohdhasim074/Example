4e27d68402c37c5230d34f810f6e552fd61b3fae
MHKhan <mhkhan04@gmail.com>
2024-01-23 12:12:36 +0530

    demo.java file edited,  index.html created

--- demo.java
+++ demo.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class demo {
       public static void main(String args[]) {
 
             System.out.println("This is testing file ");
             System.out.println("Hello, Welcome in the world of github");
+            // This is Scanner class to take input from user
+            Scanner sc = new Scanner(System.in);
+            //
+            System.out.println("Enter a number : ");
+            int a = sc.nextInt();
+
       }
 }
